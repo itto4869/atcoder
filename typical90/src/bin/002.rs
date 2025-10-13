0f42cf5bd8ed74fbe2145ba2216b92046c17a9ac
itto4869 <itto4869@gmail.com>
2025-10-13 22:14:15 +0900

    typical90: 002 is challenged

--- typical90/src/bin/002.rs
+++ typical90/src/bin/002.rs
@@ -2,5 +2,27 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+    }
+    for mask in 0..(1 << n) {
+        let mut seq = String::with_capacity(n);
+        let mut left = 0;
+        let mut right = 0;
+        for i in 0..n {
+            if mask & (1 << (n - i - 1)) == 0 {
+                seq.push('(');
+                left += 1;
+            } else {
+                seq.push(')');
+                right += 1;
+            }
+            if left < right {
+                break;
+            }
+        }
+        if left == right {
+            println!("{}", seq);
+        }
+    }
 }
