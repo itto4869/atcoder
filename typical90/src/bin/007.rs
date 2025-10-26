6539fba6adbcd712062053481ceaac92f7703505
itto4869 <itto4869@gmail.com>
2025-10-26 19:40:05 +0900

    typical90: 007 challenged

--- typical90/src/bin/007.rs
+++ typical90/src/bin/007.rs
@@ -2,5 +2,20 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        mut a: [u64; n],
+        q: usize,
+        b: [u64; q],
+    }
+    a.sort();
+    for &bi in &b {
+        let idx = a.partition_point(|&x| x < bi).min(n - 1);
+        let res = if a[idx].abs_diff(bi) > a[idx.saturating_sub(1)].abs_diff(bi) {
+            a[idx.saturating_sub(1)].abs_diff(bi)
+        } else {
+            a[idx].abs_diff(bi)
+        };
+        println!("{}", res);
+    }
 }
