48d0e40a467682e2a6d1642a56a052c8d8dfc505
itto4869 <itto4869@gmail.com>
2025-11-06 02:10:06 +0900

    typical90: 014

--- typical90/src/bin/014.rs
+++ typical90/src/bin/014.rs
@@ -2,5 +2,17 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        mut a: [u64; n],
+        mut b: [u64; n],
+    }
+    a.sort_unstable();
+    b.sort_unstable();
+    
+    let mut ans = 0;
+    for (&ai, &bi) in a.iter().zip(b.iter()) {
+        ans += ai.abs_diff(bi);
+    }
+    println!("{}", ans);
 }
