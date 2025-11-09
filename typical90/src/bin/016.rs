4cbbc313070660ce7b3f1f5e251f60cec85fec62
itto4869 <itto4869@gmail.com>
2025-11-09 20:16:21 +0900

    typical90: 016

--- typical90/src/bin/016.rs
+++ typical90/src/bin/016.rs
@@ -2,5 +2,20 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: u64,
+        mut abc: [u64; 3],
+    }
+    let mut ans = u64::MAX;
+    let l = 9999;
+    abc.sort_unstable();
+    abc.reverse();
+    for x in 0..=l {
+        for y in 0..=(l - x) {
+            if n >= (abc[0] * x + abc[1] * y) && (n - (abc[0] * x + abc[1] * y)) % abc[2] == 0 {
+                ans = ans.min(x + y + ((n - (abc[0] * x + abc[1] * y)) / abc[2]));
+            }
+        }
+    }
+    println!("{}", ans);
 }
