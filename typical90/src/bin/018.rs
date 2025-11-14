686ee83fd0e3219dfd1144148d0545958e3b6fd3
itto4869 <itto4869@gmail.com>
2025-11-14 16:31:12 +0900

    typical90: 018

--- typical90/src/bin/018.rs
+++ typical90/src/bin/018.rs
@@ -1,6 +1,30 @@
 use proconio::{fastout, input};
+use std::f64::consts::PI;
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        t: f64,
+        l: f64,
+        x: f64,
+        y: f64,
+        q: usize,
+        e: [f64; q],
+    }
+    let r = l / 2.0;
+    for &ei in &e {
+        let theta = 2.0 * PI * ei / t;
+
+        let yi = -r * theta.sin();
+        let zi = r - r * theta.cos();
+
+        let dx = x;
+        let dy = yi - y;
+        let dist = (dx * dx + dy * dy).sqrt();
+
+        let rad = zi.atan2(dist);
+        let deg = rad.to_degrees();
+
+        println!("{}", deg);
+    }
 }
