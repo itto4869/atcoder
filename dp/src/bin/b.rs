b1c6281551de9080ed089c64dd9821afe007294f
itto4869 <itto4869@gmail.com>
2025-11-14 14:49:07 +0900

    dp: b

--- dp/src/bin/b.rs
+++ dp/src/bin/b.rs
@@ -2,5 +2,17 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        k: usize,
+        h: [u64; n],
+    }
+    let mut dp = vec![1 << 60; n];
+    dp[0] = 0;
+    for i in 0..n {
+        for j in 1..=k.min(n - i - 1) {
+            dp[i + j] = dp[i + j].min(dp[i] + h[i + j].abs_diff(h[i]));
+        }
+    }
+    println!("{}", dp[n - 1]);
 }
