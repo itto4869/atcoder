11e439c319fc8d09d2d42685d3eb243baa9a1c57
itto4869 <itto4869@gmail.com>
2025-11-17 01:32:06 +0900

    edpc: c

--- dp/src/bin/c.rs
+++ dp/src/bin/c.rs
@@ -2,5 +2,27 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        abc: [[u64; 3]; n],
+    }
+    let mut dp = vec![vec![0; 3]; n + 1];
+
+    for i in 0..n {
+        for j in 0..3 {
+            for k in 0..3 {
+                if j == k {
+                    continue;
+                } else {
+                    dp[i + 1][k] = dp[i + 1][k].max(dp[i][j] + abc[i][k]);
+                }
+            }
+        }
+    }
+
+    let mut ans = 0;
+    for i in 0..3 {
+        ans = ans.max(dp[n][i]);
+    }
+    println!("{}", ans);
 }
