349b55bb56ae6435a1d5c5deee1177ec0722f883
itto4869 <itto4869@gmail.com>
2025-11-18 17:39:32 +0900

    typical90: 020

--- typical90/src/bin/020.rs
+++ typical90/src/bin/020.rs
@@ -2,5 +2,18 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        a: u128,
+        b: usize,
+        c: u128,
+    }
+    let mut n = c;
+    for _ in 1..b {
+        n *= c;
+        if n > a {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
 }
