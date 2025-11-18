2268390498592da6e990f905e5fe93b84b87ed71
itto4869 <itto4869@gmail.com>
2025-11-18 17:55:43 +0900

    edpc: d

--- dp/src/bin/d.rs
+++ dp/src/bin/d.rs
@@ -2,5 +2,21 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        weight: usize,
+        wv: [(u64, u64); n],
+    }
+    let mut dp = vec![vec![0; weight + 1]; n + 1];
+    for i in 1..=n {
+        let (w, v) = wv[i - 1];
+        for j in 1..=weight {
+            if w > j as u64 {
+                dp[i][j] = dp[i - 1][j];
+            } else {
+                dp[i][j] = dp[i - 1][j].max(dp[i - 1][j - w as usize] + v);
+            }
+        }
+    }
+    println!("{}", dp[n][weight]);
 }
