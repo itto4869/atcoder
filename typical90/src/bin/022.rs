740956345b0a492fc1de95bb232f96052fbd0b75
itto4869 <itto4869@gmail.com>
2025-11-22 01:55:29 +0900

    typical90: 022 challenged

--- typical90/src/bin/022.rs
+++ typical90/src/bin/022.rs
@@ -1,6 +1,26 @@
+use std::mem::swap;
+
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        a: u64,
+        b: u64,
+        c: u64,
+    }
+    let r = gcd(a, gcd(b, c));
+    println!("{}", (a / r - 1) + (b / r - 1) + (c / r - 1));
+}
+
+fn gcd(x: u64, y: u64) -> u64 {
+    let mut a = x.max(y);
+    let mut b = x.min(y);
+    while a > 0 && b > 0 {
+        a = a % b;
+        if a < b {
+            swap(&mut a, &mut b);
+        }
+    }
+    a
 }
