53a77297cdf740cf88e19b58a2ffc2e83b81b689
itto4869 <itto4869@gmail.com>
2025-11-24 21:34:38 +0900

    typical90: 024

--- typical90/src/bin/024.rs
+++ typical90/src/bin/024.rs
@@ -2,5 +2,28 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        k: u64,
+        a: [u64; n],
+        b: [u64; n],
+    }
+    let mut k = Some(k);
+    for (&ai, &bi) in a.iter().zip(b.iter()) {
+        if let Some(l) = k {
+            k = l.checked_sub(ai.abs_diff(bi));
+        } else {
+            break;
+        }
+    }
+
+    if let Some(l) = k {
+        if l % 2 == 0 {
+            println!("Yes");
+        } else {
+            println!("No");
+        }
+    } else {
+        println!("No");
+    }
 }
