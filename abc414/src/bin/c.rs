caa7cabc8d193387b9edb8b79ab439a7026d79b9
itto4869 <itto4869@gmail.com>
2025-11-28 20:13:08 +0900

    abc414: d

--- abc414/src/bin/c.rs
+++ abc414/src/bin/c.rs
@@ -3,8 +3,48 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        a: u8,
-        n: u128,
+        a: u64,
+        n: u64,
     }
-    
+    let mut ans = 0;
+
+    for length in 1..=12 {
+        let half_len = (length + 1) / 2;
+
+        let start = 10u64.pow(half_len - 1);
+        let end = 10u64.pow(half_len);
+
+        for i in start..end {
+            let s = i.to_string();
+            let rev_s: String = s.chars().rev().collect();
+
+            let pal_str = if length % 2 == 0 {
+                format!("{}{}", s, rev_s)
+            } else {
+                format!("{}{}", s, &rev_s[1..])
+            };
+
+            let val: u64 = pal_str.parse().unwrap();
+
+            if val > n {
+                break;
+            }
+
+            if is_palindrome_base_a(val, a) {
+                ans += val;
+            }
+        }
+    }
+
+    println!("{}", ans);
+}
+
+fn is_palindrome_base_a(mut x: u64, a: u64) -> bool {
+    let mut digits = Vec::with_capacity(64);
+    while x > 0 {
+        digits.push(x % a);
+        x /= a;
+    }
+
+    digits.iter().eq(digits.iter().rev())
 }
--- abc414/src/bin/d.rs
+++ abc414/src/bin/d.rs
@@ -1,6 +1,30 @@
+use std::collections::HashSet;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        m: usize,
+    }
+    let mut point = HashSet::new();
+    for _ in 0..n {
+        input! {
+            x: u64,
+        }
+        point.insert(x);
+    }
+    let mut point: Vec<u64> = point.into_iter().collect();
+    point.sort();
+    let mut dist = Vec::with_capacity(n);
+    for i in 0..(point.len() - 1) {
+        dist.push(point[i + 1] - point[i]);
+    }
+    dist.sort();
+
+    let mut ans = 0;
+    for i in 0..(point.len().saturating_sub(m)) {
+        ans += dist[i];
+    }
+    println!("{}", ans);
 }
