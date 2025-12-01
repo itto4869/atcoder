7580ea239bed1ca38f2e68e0a299496bfdeabae7
itto4869 <itto4869@gmail.com>
2025-12-01 22:54:50 +0900

    typical90: 027

--- typical90/src/bin/027.rs
+++ typical90/src/bin/027.rs
@@ -1,6 +1,22 @@
+use std::collections::HashSet;
+
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+    }
+    let mut set = HashSet::new();
+    for i in 0..n {
+        input! {
+            s: String,
+        }
+        if set.contains(&s) {
+            continue;
+        } else {
+            set.insert(s);
+            println!("{}", i + 1);
+        }
+    }
 }
