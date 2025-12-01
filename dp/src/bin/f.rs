6cc31b5750cc5a216ea909f233565158be2acb74
itto4869 <itto4869@gmail.com>
2025-12-02 00:31:49 +0900

    dp: f challenged

--- dp/src/bin/f.rs
+++ dp/src/bin/f.rs
@@ -1,6 +1,37 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Bytes};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        s: Bytes,
+        t: Bytes,
+    }
+    let mut dp = vec![vec![0; t.len() + 1]; s.len() + 1];
+    for i in 1..=s.len() {
+        for j in 1..=t.len() {
+            if s[i - 1] == t[j - 1] {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = dp[i - 1][j].max(dp[i][j - 1]);
+            }
+        }
+    }
+
+    let mut i = s.len();
+    let mut j = t.len();
+    let mut ans = Vec::new();
+
+    while i > 0 && j > 0 {
+        if s[i - 1] == t[j - 1] {
+            ans.push(s[i - 1] as char);
+            i -= 1;
+            j -= 1;
+        } else if dp[i][j] == dp[i - 1][j] {
+            i -= 1;
+        } else {
+            j -= 1;
+        }
+    }
+    ans.reverse();
+    println!("{}", ans.iter().collect::<String>());
 }
