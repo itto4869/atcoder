124a7255106841509a9430d9120bbd9c42f11045
itto4869 <itto4869@gmail.com>
2025-12-02 21:57:18 +0900

    typical90: 028

--- typical90/src/bin/028.rs
+++ typical90/src/bin/028.rs
@@ -1,6 +1,45 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Usize1};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+    }
+    let mut imos = vec![vec![0i64; 1001]; 1001];
+    for _ in 0..n {
+        input! {
+            lx: usize,
+            ly: usize,
+            rx: Usize1,
+            ry: Usize1,
+        }
+        imos[ly][lx] += 1;
+        imos[ly][rx + 1] -= 1;
+        imos[ry + 1][lx] -= 1;
+        imos[ry + 1][rx + 1] += 1;
+    }
+
+    for i in 0..=1000 {
+        for j in 1..=1000 {
+            imos[i][j] += imos[i][j - 1];
+        }
+    }
+
+    for j in 0..=1000 {
+        for i in 1..=1000 {
+            imos[i][j] += imos[i - 1][j];
+        }
+    }
+
+    let mut cnt = vec![0; n + 1];
+    for i in 0..=1000 {
+        for j in 0..=1000 {
+            let k = imos[i][j];
+            cnt[k as usize] += 1;
+        }
+    }
+
+    for &k in cnt.iter().skip(1) {
+        println!("{}", k);
+    }
 }
