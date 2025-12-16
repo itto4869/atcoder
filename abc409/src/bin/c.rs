d5c5681638a4bd21776179a2f8352d05cf750d40
itto4869 <itto4869@gmail.com>
2025-12-16 21:12:33 +0900

    abc409 cd

--- abc409/src/bin/c.rs
+++ abc409/src/bin/c.rs
@@ -2,5 +2,30 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        l: usize,
+    }
+    let mut point = vec![0; l];
+    let mut curr = 0;
+    point[curr] += 1;
+    for _ in 0..(n - 1) {
+        input! {
+            d: usize,
+        }
+        curr = (curr + d) % l;
+        point[curr] += 1;
+    }
+
+    let mut ans = 0u64;
+    if l % 3 != 0 {
+        println!("0");
+        return;
+    }
+
+    for i in 0..(l / 3) {
+        ans += point[i] * point[i + l / 3] * point[i + 2 * l / 3];
+    }
+
+    println!("{}", ans);
 }
--- abc409/src/bin/d.rs
+++ abc409/src/bin/d.rs
@@ -1,6 +1,51 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        t: usize,
+    }
+    for _ in 0..t {
+        input! {
+            n: usize,
+            s: Chars,
+        }
+        let mut l = 0;
+        let mut r = n - 1;
+        let mut step = 0;
+        for i in 0..(n - 1) {
+            if step == 0 {
+                if s[i] > s[i + 1] {
+                    l = i;
+                    step = 1;
+                }
+            } else if step == 1 {
+                if s[l] < s[i + 1] {
+                    r = i;
+                    step = 2;
+                    break;
+                }
+            } 
+        }
+
+        if step == 0 {
+            let ans: String = s.iter().collect();
+            println!("{}", ans);
+        } else if step == 1 {
+            let s1: String = s[0..l].iter().collect();
+            let s2: String = s[(l + 1)..].iter().collect();
+            let mut ans = s1 + &s2;
+            ans.push(s[l]);
+            println!("{}", ans);
+        } else {
+            let s1: String = s[0..l].iter().collect();
+            let s2: String = s[(l + 1)..=r].iter().collect();
+            let s3: String = s[(r + 1)..].iter().collect();
+
+            let mut ans = s1 + &s2;
+            ans.push(s[l]);
+            ans += &s3;
+            println!("{}", ans);
+        }
+    }
 }
