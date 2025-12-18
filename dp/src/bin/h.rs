a5f87ffd47001ac3bb3634e0a8d02d47964d9aad
itto4869 <itto4869@gmail.com>
2025-12-19 02:32:05 +0900

    edpc: h challenged

--- dp/src/bin/h.rs
+++ dp/src/bin/h.rs
@@ -1,6 +1,28 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Bytes};
+
+const MOD: u64 = 1_000_000_007;
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        h: usize,
+        w: usize,
+        grid: [Bytes; h],
+    }
+    let mut dp = vec![vec![0; w]; h];
+    dp[0][0] = 1;
+
+    for i in 0..h {
+        for j in 0..w {
+            if (i + 1) < h && grid[i + 1][j] == b'.' {
+                dp[i + 1][j] = (dp[i + 1][j] + dp[i][j]) % MOD;
+            }
+
+            if (j + 1) < w && grid[i][j + 1] == b'.' {
+                dp[i][j + 1] = (dp[i][j + 1] + dp[i][j]) % MOD;
+            }
+        }
+    }
+
+    println!("{}", dp[h - 1][w - 1]);
 }
