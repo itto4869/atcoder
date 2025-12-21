49c9bd4225173b9e09b5b97e12a8c2efa8cd3d4d
itto4869 <itto4869@gmail.com>
2025-12-22 01:40:46 +0900

    typical90: 033

--- typical90/src/bin/033.rs
+++ typical90/src/bin/033.rs
@@ -2,5 +2,14 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        h: usize,
+        w: usize,
+    }
+    if h < 2 || w < 2 {
+        println!("{}", h * w);
+    } else {
+        let ans = ((w + 2 - 1) / 2) * ((h + 2 - 1) / 2);
+        println!("{}", ans);
+    }
 }
