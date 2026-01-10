5f242ba96a8a43402067f3aff922069249efab4b
itto4869 <itto4869@gmail.com>
2026-01-11 02:00:30 +0900

    typical90: 038

--- typical90/src/bin/038.rs
+++ typical90/src/bin/038.rs
@@ -1,6 +1,17 @@
+use cp_library::math::numeric::GCD;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        a: u128,
+        b: u128,
+    }
+    let ans = a.lcm(b);
+    if ans > 10u128.pow(18) {
+        println!("Large");
+        return;
+    }
+
+    println!("{}", ans);
 }
