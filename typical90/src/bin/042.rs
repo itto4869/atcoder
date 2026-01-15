3be76849c4990d6c357b2e4f2e0e319cf5e2fbfe
itto4869 <itto4869@gmail.com>
2026-01-16 00:33:49 +0900

    typical90: 042

--- typical90/src/bin/042.rs
+++ typical90/src/bin/042.rs
@@ -1,6 +1,23 @@
 use proconio::{fastout, input};
 
+const MOD: usize = 1_000_000_007;
+
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        k: usize,
+    }
+    if k % 9 != 0 {
+        println!("0");
+    } else {
+        let mut dp = vec![0; k + 1];
+        dp[0] = 1;
+        for i in 1..=k {
+            let b = i.min(9);
+            for j in 1..=b {
+                dp[i] = (dp[i] + dp[i - j]) % MOD;
+            }
+        }
+        println!("{}", dp[k]);
+    }
 }
