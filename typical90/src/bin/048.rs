42650f83ff758434d28b16f0a5c94c6d93d40275
itto4869 <itto4869@gmail.com>
2026-02-06 22:13:00 +0900

    typical90: 048#

--- typical90/src/bin/048.rs
+++ typical90/src/bin/048.rs
@@ -2,5 +2,26 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        k: usize,
+    }
+    let mut s = Vec::with_capacity(2 * n);
+    for _ in 0..n {
+        input! {
+            a: u64,
+            b: u64,
+        }
+        s.push(b);
+        s.push(a - b);
+    }
+    s.sort_unstable();
+    s.reverse();
+    
+    let mut ans = 0;
+    for i in 0..k {
+        ans += s[i];
+    }
+
+    println!("{}", ans);
 }
