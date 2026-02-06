d8cf74fa3c167e03e6fc2e188e4e6171d33e4ae4
itto4869 <itto4869@gmail.com>
2026-02-06 21:35:45 +0900

    edpc: k

--- dp/src/bin/k.rs
+++ dp/src/bin/k.rs
@@ -2,5 +2,23 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; n],
+    }
+    let mut dp = vec![false; k + 1];
+    for i in 1..=k {
+        for j in 0..n {
+            if i >= a[j] {
+                dp[i] |= !dp[i - a[j]];
+            }
+        }
+    }
+
+    if dp[k] {
+        println!("First");
+    } else {
+        println!("Second");
+    }
 }
