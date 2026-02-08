1a4a4c6026f38cfd8f17dd31a9ead9320aa91f82
itto4869 <itto4869@gmail.com>
2026-02-08 20:39:28 +0900

    typical90: 050

--- typical90/src/bin/050.rs
+++ typical90/src/bin/050.rs
@@ -1,6 +1,18 @@
 use proconio::{fastout, input};
 
+const MOD: u64 = 1_000_000_007;
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        l: usize,
+    }
+    let mut dp = vec![0; 250000];
+    dp[0] = 1;
+    for i in 0..n {
+        dp[i + 1] = (dp[i + 1] + dp[i]) % MOD;
+        dp[i + l] = (dp[i + l] + dp[i]) % MOD;
+    }
+
+    println!("{}", dp[n]);
 }
