26b61e1f8b666592a4f26049208004297127b568
itto4869 <itto4869@gmail.com>
2026-02-14 01:18:31 +0900

    typical90: 052

--- typical90/src/bin/052.rs
+++ typical90/src/bin/052.rs
@@ -1,6 +1,22 @@
 use proconio::{fastout, input};
 
+const MOD: u64 = 1_000_000_007;
+
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        a: [[u64; 6]; n],
+    }
+    let mut a_sum = vec![0; n];
+    for i in 0..n {
+        a_sum[i] = a[i].iter().sum();
+    }
+
+    let mut ans = 1;
+    for &s in &a_sum {
+        ans = (ans % MOD * s % MOD) % MOD;
+    }
+
+    println!("{}", ans);
 }
