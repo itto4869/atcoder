3d7270e3bf24dc31a8aebcea7a78d574067493d9
itto4869 <itto4869@gmail.com>
2026-02-21 18:48:10 +0900

    typical90: 055

--- typical90/src/bin/055.rs
+++ typical90/src/bin/055.rs
@@ -1,6 +1,25 @@
+use itertools::Itertools;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        p: u64,
+        q: u64,
+        a: [u64; n],
+    }
+    let mut ans = 0u64;
+    for comb in a.into_iter().combinations(5) {
+        let mut m = 1;
+        for v in comb {
+            m = (m * v) % p;
+        }
+
+        if m % p == q {
+            ans += 1;
+        }
+    }
+
+    println!("{}", ans);
 }
