15942c0c5f3c5d0ecc6f0059c2333cc456d7acc7
itto4869 <itto4869@gmail.com>
2026-03-02 00:20:25 +0900

    typical90: 060 063

--- typical90/src/bin/061.rs
+++ typical90/src/bin/061.rs
@@ -1,6 +1,24 @@
+use std::collections::VecDeque;
+
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        q: usize,
+    }
+    let mut queue = VecDeque::new();
+    for _ in 0..q {
+        input! {
+            t: usize,
+            x: usize,
+        }
+        if t == 1 {
+            queue.push_front(x);
+        } else if t == 2 {
+            queue.push_back(x);
+        } else {
+            println!("{}", queue.get(x - 1).unwrap());
+        }
+    }
 }
--- typical90/src/bin/063.rs
+++ typical90/src/bin/063.rs
@@ -1,6 +1,42 @@
-use proconio::{fastout, input};
+use itertools::Itertools;
+use proconio::{fastout, input, marker::Usize1};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        h: usize,
+        w: usize,
+        grid: [[Usize1; w]; h],
+    }
+    let mut ans = 0;
+    for i in 0..h {
+        let mut count = vec![0; w * h];
+        for j in 0..w {
+            let p = grid[i][j];
+            count[p] += 1;
+            ans = ans.max(count[p]);
+        }
+    }
+
+    for n in 2..=h {
+        for comb in (0..h).combinations(n) {
+            let mut count = vec![0; w * h];
+            for j in 0..w {
+                let mut ok = true;
+                for idxs in comb.windows(2) {
+                    if grid[idxs[0]][j] != grid[idxs[1]][j] {
+                        ok = false;
+                        break;
+                    }
+                }
+                if ok {
+                    let p = grid[comb[0]][j];
+                    count[p] += n;
+                    ans = ans.max(count[p]);
+                }
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
