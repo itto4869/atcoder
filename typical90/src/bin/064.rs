73c908a0c08d339f4295a9f4cf4587ceaa19b160
itto4869 <itto4869@gmail.com>
2026-03-02 19:24:40 +0900

    typical90: 064 066

--- typical90/src/bin/064.rs
+++ typical90/src/bin/064.rs
@@ -1,6 +1,36 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Usize1};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        q: usize,
+        a: [i64; n]
+    }
+    let mut diff = vec![0; n - 1];
+    for i in 0..(n - 1) {
+        diff[i] = a[i] - a[i + 1];
+    }
+    let mut res: i64 = diff.iter().map(|&x| x.abs()).sum();
+    
+    for _ in 0..q {
+        input! {
+            l: Usize1,
+            r: Usize1,
+            v: i64,
+        }
+        let mut d = 0;
+        if l > 0 {
+            d += (diff[l - 1] - v).abs() - diff[l - 1].abs();
+            diff[l - 1] -= v;
+        }
+
+        if r < n - 1 {
+            d += (diff[r] + v).abs() - diff[r].abs();
+            diff[r] += v;
+        }
+
+        res += d;
+        println!("{}", res);
+    }
 }
--- typical90/src/bin/066.rs
+++ typical90/src/bin/066.rs
@@ -1,6 +1,33 @@
+use itertools::Itertools;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        lr: [(usize, usize); n],
+    }
+    let mut ans = 0.0;
+
+    for i in 0..n {
+        for j in (i + 1)..n {
+            let (li, ri) = lr[i];
+            let (lj, rj) = lr[j];
+
+            let mut cnt = 0;
+            let total = (ri - li + 1) * (rj - lj + 1);
+
+            for x in li..=ri {
+                for y in lj..=rj {
+                    if x > y {
+                        cnt += 1;
+                    }
+                }
+            }
+
+            ans += cnt as f64 / total as f64;
+        }
+    }
+
+    println!("{}", ans);
 }
