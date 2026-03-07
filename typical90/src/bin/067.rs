a3b3c144c72df31c34c6530b0ec7c5964f488c4d
itto4869 <itto4869@gmail.com>
2026-03-07 19:21:44 +0900

    typical90: 067

--- typical90/src/bin/067.rs
+++ typical90/src/bin/067.rs
@@ -1,6 +1,16 @@
 use proconio::{fastout, input};
+use cp_library::math::base_conversion::convert_base;
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        mut n: String,
+        k: usize,
+    }
+    for _ in 0..k {
+        n = convert_base(n, 8, 9).unwrap();
+        n = n.replace("8", "5");
+    }
+
+    println!("{}", n);
 }
