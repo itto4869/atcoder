1bc7efc27334dfa910575c1d55df7e39cdceb340
itto4869 <itto4869@gmail.com>
2026-03-11 21:37:18 +0900

    typical90: 068

--- typical90/src/bin/068.rs
+++ typical90/src/bin/068.rs
@@ -1,6 +1,32 @@
-use proconio::{fastout, input};
+use cp_library::data_structure::weighted_dsu::WeightedDsu;
+use proconio::{fastout, input, marker::Usize1};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        q: usize,
+    }
+    let mut weighted_dsu = WeightedDsu::<i64>::new(n);
+    for _ in 0..q {
+        input! {
+            t: usize,
+            x: Usize1,
+            y: Usize1,
+            v: i64,
+        }
+        if t == 0 {
+            let w = if x % 2 == 1 { -v } else { v };
+            weighted_dsu.merge(x, y, w);
+        } else {
+            if let Some(d) = weighted_dsu.diff(x, y) {
+                let bx = if x % 2 == 1 { v } else { -v };
+                let by = bx + d;
+                let ay = if y % 2 == 1 { by } else { -by };
+                println!("{}", ay);
+            } else {
+                println!("Ambiguous");
+            }
+        }
+    }
 }
