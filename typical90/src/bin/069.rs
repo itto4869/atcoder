65ccc52d6fe16ad850b9bd38303311c1bc6cbd0d
itto4869 <itto4869@gmail.com>
2026-03-14 20:38:58 +0900

    typical90: 069

--- typical90/src/bin/069.rs
+++ typical90/src/bin/069.rs
@@ -1,6 +1,41 @@
+use ac_library::ModInt1000000007;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: u64,
+        k: u64,
+    }
+    if n == 1 && k < 1 {
+        println!("0");
+        return;
+    }
+
+    if n == 2 && k < 2 {
+        println!("0");
+        return;
+    }
+
+    if n >= 3 && k < 3 {
+        println!("0");
+        return;
+    }
+
+    if n == 1 {
+        println!("{}", k);
+        return;
+    }
+
+    if n == 2 {
+        println!("{}", k * (k - 1));
+        return;
+    }
+
+    let mut ans = ModInt1000000007::new(k * (k - 1));
+    let mut exp = ModInt1000000007::new(k - 2);
+    exp = exp.pow(n - 2);
+
+    ans = ans * exp;
+    println!("{}", ans);
 }
