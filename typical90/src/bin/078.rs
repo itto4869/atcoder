2dc70d12a25a731ec08b8d21e2c716a61ea15a1d
itto4869 <itto4869@gmail.com>
2026-03-28 20:06:01 +0900

    typical90: 078

--- typical90/src/bin/078.rs
+++ typical90/src/bin/078.rs
@@ -1,6 +1,24 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Usize1};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        m: usize,
+    }
+    let mut cnt = vec![0; n];
+    for _ in 0..m {
+        input! {
+            a: Usize1,
+            b: Usize1,
+        }
+        if a < b {
+            cnt[b] += 1;
+        } else if a > b {
+            cnt[a] += 1;
+        }
+    }
+
+    let ans = cnt.iter().filter(|&&x| x == 1).count();
+    println!("{}", ans);
 }
