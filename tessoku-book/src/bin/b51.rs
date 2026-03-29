648096dd634c663cd2df6447af640dae483b41ad
itto4869 <itto4869@gmail.com>
2026-03-29 22:24:46 +0900

    tessoku-book: b51

--- tessoku-book/src/bin/b51.rs
+++ tessoku-book/src/bin/b51.rs
@@ -1,6 +1,16 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Bytes};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        s: Bytes,
+    }
+    let mut l_stack = Vec::new();
+    for i in 0..s.len() {
+        if s[i] == b'(' {
+            l_stack.push(i + 1);
+        } else {
+            println!("{} {}", l_stack.pop().unwrap(), i + 1);
+        }
+    }
 }
