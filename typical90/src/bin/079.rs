c3c09d389994e1cb516477f9c987ce0589b384b7
itto4869 <itto4869@gmail.com>
2026-04-17 19:02:11 +0900

    typical90: 079 081

--- typical90/src/bin/079.rs
+++ typical90/src/bin/079.rs
@@ -2,5 +2,37 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        h: usize,
+        w: usize,
+        mut a_grid: [[i64; w]; h],
+        b_grid: [[i64; w]; h],
+    }
+    let mut ans = 0;
+    for i in 0..(h - 1) {
+        for j in 0..(w - 1) {
+            let d = b_grid[i][j] - a_grid[i][j];
+            ans += d.abs();
+            a_grid[i][j] += d;
+            a_grid[i + 1][j] += d;
+            a_grid[i][j + 1] += d;
+            a_grid[i + 1][j + 1] += d;
+        }
+    }
+
+    let mut ok = true;
+    for i in 0..h {
+        for j in 0..w {
+            if a_grid[i][j] != b_grid[i][j] {
+                ok = false;
+            }
+        }
+    }
+
+    if ok {
+        println!("Yes");
+        println!("{}", ans);
+    } else {
+        println!("No");
+    }
 }
--- typical90/src/bin/081.rs
+++ typical90/src/bin/081.rs
@@ -2,5 +2,37 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        k: usize,
+    }
+    let mut imos = vec![vec![0u64; 5001]; 5001];
+    for _ in 0..n {
+        input! {
+            a: usize,
+            b: usize,
+        }
+        imos[a][b] += 1;
+    }
+
+    for i in 0..=5000 {
+        for j in 1..=5000 {
+            imos[i][j] += imos[i][j - 1];
+        }
+    }
+
+    for i in 1..=5000 {
+        for j in 0..=5000 {
+            imos[i][j] += imos[i - 1][j];
+        }
+    }
+
+    let mut ans = 0;
+    for i in 1..=5000 {
+        for j in 1..=5000 {
+            let cnt = imos[i][j] + imos[i.saturating_sub(k + 1)][j.saturating_sub(k + 1)] - imos[i.saturating_sub(k + 1)][j] - imos[i][j.saturating_sub(k + 1)];
+            ans = ans.max(cnt);
+        }
+    }
+    println!("{}", ans);
 }
