6b835bcf95bb12864b78867ac4e5555fe6def171
itto4869 <itto4869@gmail.com>
2026-04-20 17:25:02 +0900

    tessoku-book: a60

--- tessoku-book/src/bin/a60.rs
+++ tessoku-book/src/bin/a60.rs
@@ -1,6 +1,33 @@
+use itertools::Itertools;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+    }
+    let mut stack = Vec::new();
+    let mut ans = Vec::with_capacity(n);
+    for i in 0..n {
+        input! {
+            a: u64,
+        }
+        while let Some((top, idx)) = stack.pop() {
+            if top < a {
+                continue;
+            } else {
+                stack.push((top, idx));
+                break;
+            }
+        }
+        stack.push((a, i + 1));
+
+        if let Some(idx) = stack.len().checked_sub(2) {
+            ans.push(stack[idx].1 as isize);
+        } else {
+            ans.push(-1);
+        }
+    }
+
+    println!("{}", ans.iter().format(" "));
 }
