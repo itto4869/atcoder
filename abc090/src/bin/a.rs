8415c9abaf5324566f40f3a45687096a8aad3ce3
itto4869 <itto4869@gmail.com>
2026-05-06 18:01:56 +0900

    delete file

--- abc090/src/bin/a.rs
+++
@@ -1,9 +0,0 @@
-use proconio::{fastout, input, marker::Chars};
-
-#[fastout]
-fn main() {
-    input! {
-        grid: [Chars; 3],
-    }
-    println!("{}{}{}", grid[0][0], grid[1][1], grid[2][2]);
-}
