cbc5168bf821f769b766d1638f031bd8a6b25ce2
itto4869 <itto4869@gmail.com>
2026-06-17 00:14:55 +0900

    abc321: c

--- abc321/src/bin/c.rs
+++ abc321/src/bin/c.rs
@@ -1,8 +1,29 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Usize1};
 
 #[fastout]
 fn main() {
     input! {
-        k: usize
+        k: Usize1
+    }
+    
+    let mut ans = Vec::new();
+    dfs(k, 0, &mut ans);
+    ans.sort_unstable();
+    println!("{}", ans[k]);
+}
+
+fn dfs(k: usize, x: usize, ans: &mut Vec<usize>) {
+    if x == 0 {
+        for i in 1..=9 {
+            ans.push(i);
+            dfs(k, i, ans);
+        }
+    } 
+    else {
+        let bottom = x % 10;
+        for i in 0..bottom {
+            ans.push(10 * x + i);
+            dfs(k, 10 * x + i, ans);
+        }
     }
 }
