a766a37689de24f2c6aa92ec63888e104b21b3e3
itto4869 <itto4869@gmail.com>
2026-07-11 16:40:51 +0900

    arc218: a

---
+++ arc218/src/bin/a.rs
@@ -0,0 +1,13 @@
+use std::collections::HashMap;
+
+use proconio::{fastout, input};
+
+#[fastout]
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        a: [[usize; m]; n],
+    }
+    
+}
