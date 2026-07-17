709f15e917dd295f2eadc3ff62e05927fc498357
itto4869 <itto4869@gmail.com>
2026-07-18 02:23:28 +0900

    typical90: 085

--- typical90/src/bin/085.rs
+++ typical90/src/bin/085.rs
@@ -2,5 +2,25 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        k: usize,
+    }
+    let mut ans = 0usize;
+    let mut a = 1usize;
+    while a * a <= k {
+        if k % a == 0 {
+            let mut b = a;
+            while b * b <= k / a {
+                if (k / a) % b == 0 {
+                    ans += 1;
+                }
+
+                b += 1;
+            }
+        }
+
+        a += 1;
+    }
+
+    println!("{}", ans);
 }
