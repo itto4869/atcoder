edb7f8bbd5bcc55304164e32b474b35fd4553f4a
itto4869 <itto4869@gmail.com>
2026-08-17 01:19:17 +0900

    typical90: 002

--- typical90/src/bin/002.rs
+++ typical90/src/bin/002.rs
@@ -1,6 +1,39 @@
+use itertools::Itertools;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+    }
+    let mut s = Vec::new();
+    let mut v = Vec::new();
+    dfs(&mut s, 0, 0, n, &mut v);
+    v.sort_unstable();
+    println!("{}", v.iter().format("\n"));
+}
+
+fn dfs(s: &mut Vec<char>, l_cnt: usize, r_cnt: usize, n: usize, v: &mut Vec<String>) {
+    if s.len() == n {
+        if l_cnt != r_cnt {
+            return;
+        } else {
+            let ss: String = s.iter().collect();
+            v.push(ss);
+        }
+    } else {
+        if l_cnt == r_cnt {
+            s.push('(');
+            dfs(s, l_cnt + 1, r_cnt, n, v);
+            s.pop();
+        } else {
+            s.push('(');
+            dfs(s, l_cnt + 1, r_cnt, n, v);
+
+            s.pop();
+            s.push(')');
+            dfs(s, l_cnt, r_cnt + 1, n, v);
+            s.pop();
+        }
+    }
 }
