a4054a7370e675aae8d20e6d01cab4530695756f
itto4869 <itto4869@gmail.com>
2026-08-20 01:44:59 +0900

    dp: d, e

--- dp/src/bin/d.rs
+++ dp/src/bin/d.rs
@@ -2,5 +2,23 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        w: usize,
+        wv: [(usize, usize); n],
+    }
+    let mut dp = vec![vec![0; w + 1]; n + 1];
+    for i in 1..=n {
+        let (wi, vi) = wv[i - 1];
+        for k in 0..=w {
+            if k < wi {
+                dp[i][k] = dp[i - 1][k];
+            } else {
+                dp[i][k] = dp[i - 1][k].max(dp[i - 1][k - wi] + vi);
+            }
+        }
+    }
+
+    let ans = dp[n][w];
+    println!("{}", ans);
 }
--- dp/src/bin/e.rs
+++ dp/src/bin/e.rs
@@ -2,5 +2,29 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        w: usize,
+        wv: [(usize, usize); n],
+    }
+    let v = 100000;
+    let mut dp = vec![vec![w + 1; v + 1]; n + 1];
+    dp[0][0] = 0;
+    for i in 1..=n {
+        let (wi, vi) = wv[i - 1];
+        for k in 0..=v {
+            if k < vi {
+                dp[i][k] = dp[i - 1][k];
+            } else {
+                dp[i][k] = dp[i - 1][k].min(dp[i - 1][k - vi] + wi);
+            }
+        }
+    }
+
+    for i in (0..=v).rev() {
+        if dp[n][i] <= w {
+            println!("{}", i);
+            return;
+        }
+    }
 }
