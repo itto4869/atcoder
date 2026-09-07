6ce80bae45913a1a3d80d7c394655f2ddb8d7a97
itto4869 <itto4869@gmail.com>
2026-09-08 05:40:21 +0900

    typical90: 011, 012

--- typical90/src/bin/011.rs
+++ typical90/src/bin/011.rs
@@ -2,5 +2,26 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        mut dcs: [(usize, usize, usize); n],
+    }
+    let &max_d = dcs.iter().map(|(d, _, _)| d).max().unwrap();
+    dcs.sort_unstable();
+    let mut dp = vec![vec![0; n + 1]; 10001];
+    for i in 1..=max_d {
+        for j in 1..=n {
+            let (d, c, s) = dcs[j - 1];
+            if i < c {
+                dp[i][j] = dp[i][j - 1];
+            } else if i <= d {
+                dp[i][j] = dp[i][j - 1].max(dp[i - c][j - 1] + s);
+            } else {
+                dp[i][j] = dp[i][j - 1].max(dp[d][j])
+            }
+        }
+    }
+
+    let ans = dp[max_d][n];
+    println!("{}", ans);
 }
--- typical90/src/bin/012.rs
+++ typical90/src/bin/012.rs
@@ -1,6 +1,46 @@
-use proconio::{fastout, input};
+use ac_library::Dsu;
+use cp_library::grid::neighbors4;
+use proconio::{fastout, input, marker::Usize1};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        h: usize,
+        w: usize,
+        q: usize,
+    }
+    let mut dsu = Dsu::new(h * w);
+    let mut grid = vec![vec![0; w]; h];
+    for _ in 0..q {
+        input! {
+            t: usize,
+        }
+        if t == 1 {
+            input! {
+                r: Usize1,
+                c: Usize1,
+            }
+
+            grid[r][c] = 1;
+            let neighbors = neighbors4(r, c, h, w);
+            for (nr, nc) in neighbors {
+                if grid[nr][nc] == 1 {
+                    dsu.merge(w * r + c, w * nr + nc);
+                }
+            }
+        } else {
+            input! {
+                ra: Usize1,
+                ca: Usize1,
+                rb: Usize1,
+                cb: Usize1,
+            }
+
+            if grid[ra][ca] == 1 && grid[rb][cb] == 1 && dsu.same(w * ra + ca, w * rb + cb) {
+                println!("Yes");
+            } else {
+                println!("No");
+            }
+        }
+    }
 }
