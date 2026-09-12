b7674e6f02fd321404d7381b546cb527046cdb42
itto4869 <itto4869@gmail.com>
2026-09-12 20:43:11 +0900

    typical90: 015

--- typical90/src/bin/015.rs
+++ typical90/src/bin/015.rs
@@ -1,6 +1,21 @@
+use cp_library::math::combinations::Combination;
 use proconio::{fastout, input};
 
+const MOD: u64 = 1_000_000_007;
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+    }
+    let comb = Combination::new(n, MOD);
+    for k in 1..=n {
+        let mut ans = 0;
+        for a in 1..=((n + k - 1) / k) {
+            let d = n - (k - 1) * (a - 1);
+            let cnt = comb.n_c_r(d, a);
+            ans = (ans + cnt) % MOD;
+        }
+
+        println!("{}", ans);
+    }
 }
