177938d4aee4f4ce6107b3d1afa287f7149a8c0c
itto4869 <itto4869@gmail.com>
2026-09-16 22:47:14 +0900

    typical90: 016, 018

--- typical90/src/bin/016.rs
+++ typical90/src/bin/016.rs
@@ -2,5 +2,25 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        mut abc: [usize; 3],
+    }
+    abc.sort_unstable();
+    let mut ans = usize::MAX;
+    for i in 0..10000 {
+        for j in 0..10000 {
+            let cnt = abc[2] * i + abc[1] * j;
+            if cnt > n {
+                break;
+            }
+
+            let res = n - cnt;
+            if (res % abc[0]) == 0 {
+                ans = ans.min(i + j + res / abc[0]);
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
--- typical90/src/bin/018.rs
+++ typical90/src/bin/018.rs
@@ -1,6 +1,31 @@
+use std::f64::consts::PI;
+
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        t: f64,
+        l: f64,
+        x: f64,
+        y: f64,
+        q: usize,
+    }
+    for _ in 0..q {
+        input! {
+            e: f64,
+        }
+        let e = e % t;
+        let rad = 2.0 * PI * (e / t);
+
+        let yt = -rad.sin() * (l / 2.0);
+        let zt = (rad.cos() * (l / 2.0) - (l / 2.0)).abs();
+
+        let (dx, dy, dz) = (x, (y - yt).abs(), zt);
+        let r = (dx * dx + dy * dy).sqrt();
+
+        let theta = dz.atan2(r) * (180.0 / PI);
+
+        println!("{:.12}", theta);
+    }
 }
