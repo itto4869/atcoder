d208f59ed7766b250a0ad7fc4efafc88e620099f
itto4869 <itto4869@gmail.com>
2026-09-23 22:42:03 +0900

    tessoku-book: a58

--- tessoku-book/src/bin/a58.rs
+++ tessoku-book/src/bin/a58.rs
@@ -1,6 +1,38 @@
+use ac_library::Segtree;
+use ac_library::Max;
+use proconio::marker::Usize1;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        q: usize,
+    }
+    let mut segtree = Segtree::<Max<usize>>::new(n);
+    for i in 0..n {
+        segtree.set(i, 0);
+    }
+
+    for _ in 0..q {
+        input! {
+            t: usize,
+        }
+        if t == 1 {
+            input! {
+                p: Usize1,
+                x: usize,
+            }
+
+            segtree.set(p, x);
+        } else {
+            input! {
+                l: Usize1,
+                r: Usize1
+            }
+
+            let res = segtree.prod(l..r);
+            println!("{}", res);
+        }
+    }
 }
