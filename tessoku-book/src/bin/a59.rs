93b0bf27aaf663e250c0389fbbf2d15d12901bcc
itto4869 <itto4869@gmail.com>
2026-09-24 01:31:42 +0900

    tessoku-book: a59

--- tessoku-book/src/bin/a59.rs
+++ tessoku-book/src/bin/a59.rs
@@ -1,6 +1,36 @@
+use ac_library::Segtree;
+use ac_library::Additive;
+use proconio::marker::Usize1;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        q: usize,
+    }
+    let mut segtree = Segtree::<Additive<usize>>::new(n);
+    for i in 0..n {
+        segtree.set(i, 0);
+    }
+
+    for _ in 0..q {
+        input! {
+            t: usize,
+        }
+        if t == 1 {
+            input! {
+                p: Usize1,
+                x: usize,
+            }
+            segtree.set(p, x);
+        } else {
+            input! {
+                l: Usize1,
+                r: Usize1
+            }
+            let res = segtree.prod(l..r);
+            println!("{}", res);
+        }
+    }
 }
