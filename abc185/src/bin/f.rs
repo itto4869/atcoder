e98a11f0fe347f49c7b9591ddd8af1235ec0834b
itto4869 <itto4869@gmail.com>
2026-09-24 01:48:29 +0900

    abc185: f

--- abc185/src/bin/f.rs
+++ abc185/src/bin/f.rs
@@ -1,6 +1,32 @@
-use proconio::{fastout, input};
+use ac_library::{BitwiseXor, Segtree};
+use proconio::{fastout, input, marker::Usize1};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        q: usize,
+        a: [usize; n],
+    }
+    let mut segtree = Segtree::<BitwiseXor<usize>>::from_iter(a.into_iter());
+    for _ in 0..q {
+        input! {
+            t: usize,
+        }
+        if t == 1 {
+            input! {
+                x: Usize1,
+                y: usize,
+            }
+            let p = segtree.get(x);
+            segtree.set(x, p ^ y);
+        } else {
+            input! {
+                x: Usize1,
+                y: Usize1
+            }
+            let res = segtree.prod(x..=y);
+            println!("{}", res);
+        }
+    }
 }
