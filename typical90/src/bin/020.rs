0f24a8cf9e4970432a6791c7429b8339100ec76a
itto4869 <itto4869@gmail.com>
2026-09-26 20:47:37 +0900

    typical90: 020

--- typical90/src/bin/020.rs
+++ typical90/src/bin/020.rs
@@ -1,6 +1,22 @@
+use cp_library::yes_no;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        a: usize,
+        b: usize,
+        c: usize,
+    }
+    let mut x = 1;
+    let mut ok = false;
+    for _ in 0..b {
+        x = x * c;
+        if x > a {
+            ok = true;
+            break;
+        }
+    }
+
+    yes_no!(ok);
 }
